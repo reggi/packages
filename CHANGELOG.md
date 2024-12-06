30ebd8a052fdd3962b8133048202487bfd064ea7
Thomas Reggi <thomas@reggi.com>
2024-12-06 10:02:19 -0500

    feat: package to add exports property to package json based on glob

---
+++ CHANGELOG.md
@@ -0,0 +1,34 @@
+# Changelog
+
+## [1.2.1](https://github.com/reggi/ts-starter/compare/v1.2.0...v1.2.1) (2024-11-27)
+
+
+### Bug Fixes
+
+* add repository url for provenance ([a6c259d](https://github.com/reggi/ts-starter/commit/a6c259d2ac6e7503f7d450d81ba6e73db57887a7))
+
+## [1.2.0](https://github.com/reggi/ts-starter/compare/v1.1.0...v1.2.0) (2024-11-27)
+
+
+### Features
+
+* init ([82178e1](https://github.com/reggi/ts-starter/commit/82178e10bba716c9bc326c3f834fc15f021c190a))
+
+
+### Bug Fixes
+
+* rollback version ([8735124](https://github.com/reggi/ts-starter/commit/8735124a0ece107d8a1c33dc6b1d2a19a7ad9b89))
+
+## [1.1.0](https://github.com/reggi/ts-starter/compare/v1.0.0...v1.1.0) (2024-11-27)
+
+
+### Features
+
+* init ([82178e1](https://github.com/reggi/ts-starter/commit/82178e10bba716c9bc326c3f834fc15f021c190a))
+
+## 1.0.0 (2024-11-27)
+
+
+### Features
+
+* init ([82178e1](https://github.com/reggi/ts-starter/commit/82178e10bba716c9bc326c3f834fc15f021c190a))
---
+++ src/bin.ts
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+import fs from 'node:fs'
+import path from 'node:path'
+import {globSync} from 'glob'
+
+const argv = process.argv.slice(2)
+const files = argv
+
+if (argv.includes('--help')) {
+  console.log(
+    `
+Usage: add-exports [glob-pattern]
+Description: Add exports field to package.json based on the files found by the glob pattern
+`.trim(),
+  )
+  process.exit(0)
+}
+
+if (!files) {
+  throw new Error('Please provide a glob pattern to search for')
+}
+
+const resolvedFiles = files.length === 1 ? globSync(files[0], {cwd: process.cwd()}) : files
+
+const x = resolvedFiles.map(rel => {
+  const extname = `.${path.basename(rel).split('.').slice(1).join('.')}`
+  const basename = path.basename(rel, extname)
+  const key = basename === 'index' ? '.' : `./${basename}`
+  const keyword = extname === '.cjs' ? 'require' : extname === '.js' ? 'import' : undefined
+  return {extname, key, rel, keyword}
+})
+
+const grouped = Object.entries(Object.groupBy(x, v => v.key)).sort(([a], [b]) =>
+  (a as string).localeCompare(b as string),
+)
+
+const exportsProperty = Object.fromEntries(
+  grouped.map(([key, value]) => {
+    return [key, Object.fromEntries(value?.filter(v => v.keyword).map(v => [v.keyword, v.rel]) || [])]
+  }),
+)
+
+const packagePath = path.join(process.cwd(), './package.json')
+const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf-8'))
+packageJson.exports = exportsProperty
+fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2))
