ab01d334879780638c9dd3113bce448b443db02a
Thomas Reggi <thomas@reggi.com>
2024-12-09 02:35:29 -0500

    chore: adds dknevee test

--- workspaces/knevee/test/bin.test.ts
+++ workspaces/knevee/test/bin.test.ts
@@ -66,149 +66,156 @@ export const invalid = (name: string, commands: string[]) =>
     })
   })
 
-await invalid('throws on depcheck failure', [
-  'tsx ./src/bin.ts ./examples/dep-check-invalid.ts tea 32', // dep-check
-])
+function runBin (bin) {
+  describe(bin, async () => {
+    await invalid('throws on depcheck failure', [
+      `${bin} ./examples/dep-check-invalid.ts tea 32`, // dep-check
+    ])
+    
+    await valid('valid stdout', [
+      `${bin} ./examples stdin-uppercase woof`,
+      `${bin} ./examples/stdin-uppercase.ts woof`,
+      './examples/stdin-uppercase.ts woof',
+    ])
+    
+    await valid('vaid stdin', [
+      withStdin(`${bin} ./examples stdin-uppercase`, ['meow', 'woof']),
+      withStdin(`${bin} ./examples/stdin-uppercase.ts`, ['meow', 'woof']),
+      withStdin('./examples/stdin-uppercase.ts', ['meow', 'woof']),
+    ])
+    
+    // dir list example
+    await valid('dir list no index', [`${bin} ./examples/no-index`])
+    
+    // help example
+    await valid('show knevee help', [`${bin} --help`])
+    
+    // no target
+    await invalid('no knevee target', [`${bin}`])
+    
+    // no command found
+    await invalid('no command found', [`${bin} ./examples/empty-dir`])
+    
+    // subcommand match
+    await valid('subcommand match', [
+      `${bin} ./examples/no-index`,
+      `${bin} ./examples/no-index xxx`,
+      `${bin} --cwd=examples/no-index`,
+      `${bin} --cwd=examples/no-index xxx`,
+    ])
+    
+    // UserError
+    await invalid('user error', [
+      `${bin} ./examples/throws.ts xxx 111`,
+      'tsx ./examples/throws-functional.ts xxx 111',
+    ])
+    
+    // knevee error
+    await invalid('knevee error', [`KNEVEE_THROW=true ${bin} ./examples/exports.ts`])
+    
+    // knevee error
+    await invalid('knevee error', [`KNEVEE_THROW=true ${bin} ./examples/empty.ts`])
+    
+    await valid('command help', [
+      `${bin} ./examples functional --help`,
+      `${bin} ./examples/functional.ts --help`,
+      './examples/functional.ts --help',
+    ])
+    
+    // normal kneve error
+    await invalid('command arg error', [
+      // ----- esm -----
+      // command
+      `${bin} ./examples command tea`,
+      `${bin} ./examples/command.ts tea`,
+      './examples/command.ts tea',
+      // functional
+      `${bin} ./examples functional tea`,
+      `${bin} ./examples/functional.ts tea`,
+      'tsx ./examples/functional.ts tea',
+      './examples/functional.ts tea',
+      // proxy
+      `${bin} ./examples proxy tea`,
+      `${bin} ./examples/proxy.ts tea`,
+      './examples/proxy.ts tea',
+      // exports
+      `${bin} ./examples exports tea`,
+      `${bin} ./examples/exports.ts tea`,
+      './examples/exports.ts tea',
+      // exports-symlink
+      `${bin} ./examples exports-symlink tea`,
+      `${bin} ./examples/exports-symlink.ts tea`,
+      './examples/exports-symlink.ts tea',
+      // extra
+      `${bin} ./examples tea`, // index
+      `${bin} ./examples/dep-check.ts tea`, // dep-check
+      // ----- cjs -----
+      // command-cjs
+      `${bin} ./examples command-cjs tea`,
+      `${bin} ./examples/command-cjs.cjs tea`,
+      './examples/command-cjs.cjs tea',
+      // proxy-cjs
+      `${bin} ./examples proxy-cjs tea`,
+      `${bin} ./examples/proxy-cjs.cjs tea`,
+      './examples/proxy-cjs.cjs tea',
+      // exports-cjs
+      `${bin} ./examples exports-cjs tea`,
+      `${bin} ./examples/exports-cjs.cjs tea`,
+      './examples/exports-cjs.cjs tea',
+      // functional-cjs
+      `${bin} ./examples functional-cjs tea`,
+      `${bin} ./examples/functional-cjs.cjs tea`,
+      'tsx ./examples/functional-cjs.cjs tea', // extra functional call
+      './examples/functional-cjs.cjs tea',
+    ])
+    
+    await valid('successful command', [
+      // ----- esm -----
+      // command
+      `${bin} ./examples command tea 32`,
+      `${bin} ./examples/command.ts tea 32`,
+      './examples/command.ts tea 32',
+      // functional
+      `${bin} ./examples functional tea 32`,
+      `${bin} ./examples/functional.ts tea 32`,
+      'tsx ./examples/functional.ts tea 32',
+      './examples/functional.ts tea 32',
+      // proxy
+      `${bin} ./examples proxy tea 32`,
+      `${bin} ./examples/proxy.ts tea 32`,
+      './examples/proxy.ts tea 32',
+      // exports
+      `${bin} ./examples exports tea 32`,
+      `${bin} ./examples/exports.ts tea 32`,
+      './examples/exports.ts tea 32',
+      // exports-symlink
+      `${bin} ./examples exports-symlink tea 32`,
+      `${bin} ./examples/exports-symlink.ts tea 32`,
+      './examples/exports-symlink.ts tea 32',
+      // extra
+      `${bin} ./examples tea 32`, // index
+      `${bin} ./examples/dep-check.ts tea 32`, // dep-check
+      // ----- cjs -----
+      // command-cjs
+      `${bin} ./examples command-cjs tea 32`,
+      `${bin} ./examples/command-cjs.cjs tea 32`,
+      './examples/command-cjs.cjs tea 32',
+      // proxy-cjs
+      `${bin} ./examples proxy-cjs tea 32`,
+      `${bin} ./examples/proxy-cjs.cjs tea 32`,
+      './examples/proxy-cjs.cjs tea 32',
+      // exports-cjs
+      `${bin} ./examples exports-cjs tea 32`,
+      `${bin} ./examples/exports-cjs.cjs tea 32`,
+      './examples/exports-cjs.cjs tea 32',
+      // functional-cjs
+      `${bin} ./examples functional-cjs tea 32`,
+      `${bin} ./examples/functional-cjs.cjs tea 32`,
+      'tsx ./examples/functional-cjs.cjs tea 32', // extra functional call
+      './examples/functional-cjs.cjs tea 32',
+    ])  
+  })
+}
 
-await valid('valid stdout', [
-  'tsx ./src/bin.ts ./examples stdin-uppercase woof',
-  'tsx ./src/bin.ts ./examples/stdin-uppercase.ts woof',
-  './examples/stdin-uppercase.ts woof',
-])
-
-await valid('vaid stdin', [
-  withStdin('tsx ./src/bin.ts ./examples stdin-uppercase', ['meow', 'woof']),
-  withStdin('tsx ./src/bin.ts ./examples/stdin-uppercase.ts', ['meow', 'woof']),
-  withStdin('./examples/stdin-uppercase.ts', ['meow', 'woof']),
-])
-
-// dir list example
-await valid('dir list no index', ['tsx ./src/bin.ts ./examples/no-index'])
-
-// help example
-await valid('show knevee help', ['tsx ./src/bin.ts --help'])
-
-// no target
-await invalid('no knevee target', ['tsx ./src/bin.ts'])
-
-// no command found
-await invalid('no command found', [`tsx ./src/bin.ts ./examples/empty-dir`])
-
-// subcommand match
-await valid('subcommand match', [
-  'tsx ./src/bin.ts ./examples/no-index',
-  'tsx ./src/bin.ts ./examples/no-index xxx',
-  'tsx ./src/bin.ts --cwd=examples/no-index',
-  'tsx ./src/bin.ts --cwd=examples/no-index xxx',
-])
-
-// UserError
-await invalid('user error', [
-  'tsx ./src/bin.ts ./examples/throws.ts xxx 111',
-  'tsx ./examples/throws-functional.ts xxx 111',
-])
-
-// knevee error
-await invalid('knevee error', [`KNEVEE_THROW=true tsx ./src/bin.ts ./examples/exports.ts`])
-
-// knevee error
-await invalid('knevee error', [`KNEVEE_THROW=true tsx ./src/bin.ts ./examples/empty.ts`])
-
-await valid('command help', [
-  'tsx ./src/bin.ts ./examples functional --help',
-  'tsx ./src/bin.ts ./examples/functional.ts --help',
-  './examples/functional.ts --help',
-])
-
-// normal kneve error
-await invalid('command arg error', [
-  // ----- esm -----
-  // command
-  'tsx ./src/bin.ts ./examples command tea',
-  'tsx ./src/bin.ts ./examples/command.ts tea',
-  './examples/command.ts tea',
-  // functional
-  'tsx ./src/bin.ts ./examples functional tea',
-  'tsx ./src/bin.ts ./examples/functional.ts tea',
-  'tsx ./examples/functional.ts tea',
-  './examples/functional.ts tea',
-  // proxy
-  'tsx ./src/bin.ts ./examples proxy tea',
-  'tsx ./src/bin.ts ./examples/proxy.ts tea',
-  './examples/proxy.ts tea',
-  // exports
-  'tsx ./src/bin.ts ./examples exports tea',
-  'tsx ./src/bin.ts ./examples/exports.ts tea',
-  './examples/exports.ts tea',
-  // exports-symlink
-  'tsx ./src/bin.ts ./examples exports-symlink tea',
-  'tsx ./src/bin.ts ./examples/exports-symlink.ts tea',
-  './examples/exports-symlink.ts tea',
-  // extra
-  'tsx ./src/bin.ts ./examples tea', // index
-  'tsx ./src/bin.ts ./examples/dep-check.ts tea', // dep-check
-  // ----- cjs -----
-  // command-cjs
-  'tsx ./src/bin.ts ./examples command-cjs tea',
-  'tsx ./src/bin.ts ./examples/command-cjs.cjs tea',
-  './examples/command-cjs.cjs tea',
-  // proxy-cjs
-  'tsx ./src/bin.ts ./examples proxy-cjs tea',
-  'tsx ./src/bin.ts ./examples/proxy-cjs.cjs tea',
-  './examples/proxy-cjs.cjs tea',
-  // exports-cjs
-  'tsx ./src/bin.ts ./examples exports-cjs tea',
-  'tsx ./src/bin.ts ./examples/exports-cjs.cjs tea',
-  './examples/exports-cjs.cjs tea',
-  // functional-cjs
-  'tsx ./src/bin.ts ./examples functional-cjs tea',
-  'tsx ./src/bin.ts ./examples/functional-cjs.cjs tea',
-  'tsx ./examples/functional-cjs.cjs tea', // extra functional call
-  './examples/functional-cjs.cjs tea',
-])
-
-await valid('successful command', [
-  // ----- esm -----
-  // command
-  'tsx ./src/bin.ts ./examples command tea 32',
-  'tsx ./src/bin.ts ./examples/command.ts tea 32',
-  './examples/command.ts tea 32',
-  // functional
-  'tsx ./src/bin.ts ./examples functional tea 32',
-  'tsx ./src/bin.ts ./examples/functional.ts tea 32',
-  'tsx ./examples/functional.ts tea 32',
-  './examples/functional.ts tea 32',
-  // proxy
-  'tsx ./src/bin.ts ./examples proxy tea 32',
-  'tsx ./src/bin.ts ./examples/proxy.ts tea 32',
-  './examples/proxy.ts tea 32',
-  // exports
-  'tsx ./src/bin.ts ./examples exports tea 32',
-  'tsx ./src/bin.ts ./examples/exports.ts tea 32',
-  './examples/exports.ts tea 32',
-  // exports-symlink
-  'tsx ./src/bin.ts ./examples exports-symlink tea 32',
-  'tsx ./src/bin.ts ./examples/exports-symlink.ts tea 32',
-  './examples/exports-symlink.ts tea 32',
-  // extra
-  'tsx ./src/bin.ts ./examples tea 32', // index
-  'tsx ./src/bin.ts ./examples/dep-check.ts tea 32', // dep-check
-  // ----- cjs -----
-  // command-cjs
-  'tsx ./src/bin.ts ./examples command-cjs tea 32',
-  'tsx ./src/bin.ts ./examples/command-cjs.cjs tea 32',
-  './examples/command-cjs.cjs tea 32',
-  // proxy-cjs
-  'tsx ./src/bin.ts ./examples proxy-cjs tea 32',
-  'tsx ./src/bin.ts ./examples/proxy-cjs.cjs tea 32',
-  './examples/proxy-cjs.cjs tea 32',
-  // exports-cjs
-  'tsx ./src/bin.ts ./examples exports-cjs tea 32',
-  'tsx ./src/bin.ts ./examples/exports-cjs.cjs tea 32',
-  './examples/exports-cjs.cjs tea 32',
-  // functional-cjs
-  'tsx ./src/bin.ts ./examples functional-cjs tea 32',
-  'tsx ./src/bin.ts ./examples/functional-cjs.cjs tea 32',
-  'tsx ./examples/functional-cjs.cjs tea 32', // extra functional call
-  './examples/functional-cjs.cjs tea 32',
-])
+await runBin('tsx ./src/bin.ts')
+await runBin('./bins/dknevee')
